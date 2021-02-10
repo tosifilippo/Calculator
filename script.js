a129f81055e7116819a0fc95e51007ed3a250bb3
filippo <tosifilippo@outlook.it>
2021-02-10 16:49:20 +0100

    joined buttons and display container in a single grid, arranged the buttons and text space properly

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function divide(num1, num2) {
     if (num2 != 0) {
         return Math.round((num1 / num2 + Number.EPSILON) * 100) / 100;
     } else {
-        return "Ci hai provato! Adesso non funziono più :(";
+        return "Ci hai provato! :)";
     };
 };
 

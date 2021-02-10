ac51f084713d96f1219c481ff0e9e393f15e8317
filippo <tosifilippo@outlook.it>
2021-02-10 22:37:01 +0100

    main function are refactored to look at wether the stored values properties exist or not, instead of the display content. This solves quite a few bugs.

--- script.js
+++ script.js
@@ -10,17 +10,15 @@ window.addEventListener('keydown', function(e){
 });
 
 let storedValues = {
-    operator : "+",
-    num1 : 0,
-    num2 : 0,
 };
 
 function clear() {  
-    display.innerText = "0";
-    subDisplay.innerText = "0";
-    storedValues.num1 = 0;
-    storedValues.num2 = 0;
-    storedValues.operator = "+";
+    display.innerText = "";
+    subDisplay.innerText = "";
+    delete storedValues.num1;
+    delete storedValues.num2;
+    delete storedValues.operator;
+    delete storedValues.result; 
 };
 
 function backSpace() {
@@ -30,15 +28,12 @@ function backSpace() {
 
 //function triggers when number buttons are clicked.
 function displayNumber(value) {
-    //if display shows correct result of an operation only the number clicked is displayed
-    //and stored values are reset.
-    if (Number(display.innerText) === operate(storedValues.operator, storedValues.num1, storedValues.num2)) {
-        display.innerText = value;
-        storedValues.num1 = 0;
-        storedValues.num2 = 0;
-        storedValues.operator = "+";
+    // if a result already exists, it is deleted and display starts from new input
+    if ((storedValues.result != null) || (display.innerText == "0")) {
+        display.innerText = value; 
+        delete storedValues.result;
     } else {
-        // the number is concatenated in the display.
+        // input is added to the display
         display.innerText += value;
     };
     // "." button is disabled if already included in the display.
@@ -51,33 +46,38 @@ function displayNumber(value) {
 
 // function triggers when operator buttons are clicked.
 function displayOperator(value) { 
-    // if display shows correct result of an operation, it becomes the next first operand, it is moved to
-    // subtotal display, the selected operator is stored and display is cleared.
-    if (Number(display.innerText) === operate(storedValues.operator, storedValues.num1, storedValues.num2)) {
+    // if first operand doesn't exist, it is taken from display, shown as subtotal,
+    // operator is stored, display is emptied
+    if (storedValues.num1 == null) {
         storedValues.num1 = Number(display.innerText);
         subDisplay.innerText = storedValues.num1;
         storedValues.operator = value;
         display.innerText = "";
     } else {
-        // value on display becomes second operand, operate function is ran and result is displayed and
-        // stored as next first operand. Finally the selected operator is stored for next operation.
+        // second operator is taken from display, operate function is run, result
+        // is stored as subtotal for next operation, display is emptied, next operator
+        // is stored, second operator is deleted.
         storedValues.num2 = Number(display.innerText);
         storedValues.num1 = operate(storedValues.operator, storedValues.num1, storedValues.num2);
         subDisplay.innerText = storedValues.num1;
         display.innerText = "";
         storedValues.operator = value;
+        delete storedValues.num2;
     };
 };
 
 // function triggers when "=" is clicked.
 function displayResults() {
-    // if display shows correct result of an operation, nothing happens. 
-    if (Number(display.innerText) === operate(storedValues.operator, storedValues.num1, storedValues.num2)) {
-        display.innerText = operate(storedValues.operator, storedValues.num1, storedValues.num2);
+    // if result exist it stays on display
+    if (storedValues.result != null) {
+        storedValues.result = display.innerText
     } else {
-        // display is stored as second operand and operate function is ran.
+        // second operand is taken from display, operate functio is run and result is 
+        // stored and shown in display, first operand is deleted.
         storedValues.num2 = Number(display.innerText);
         display.innerText = operate(storedValues.operator, storedValues.num1, storedValues.num2);
+        storedValues.result = display.innerText
+        delete storedValues.num1;
     };
 };
 
@@ -97,7 +97,7 @@ function divide(num1, num2) {
     if (num2 != 0) {
         return Math.round((num1 / num2 + Number.EPSILON) * 100) / 100;
     } else {
-        return "Ci hai provato! :)";
+        return "Ci hai provato!";
     };
 };
 
